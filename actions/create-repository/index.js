c03caf162ed17ed78322194b2c07ce69b764465f
Thomas Dupoiron <tdupoiron@github.com>
2022-12-21 15:15:04 +0100

    add branch protection

--- actions/create-repository/index.js
+++ actions/create-repository/index.js
@@ -6,16 +6,42 @@ const octokit = new Octokit({
 });
 
 // Create a new repository
-function createRepository(organization, name, visibility) {
+async function createRepository(organization, name, visibility) {
 
-    octokit.repos.createInOrg({
+    return octokit.repos.createInOrg({
         org: organization,
         name: name,
         visibility: visibility,
+        auto_init: true,
     }).then((response) => {
         core.setOutput("repository_id", response.data.id);
     }).catch((error) => {
         core.setFailed(error.response.data);
+    });
+
+}
+
+// Set branch protection on main
+async function setBranchProtection(organization, name, branch) {
+
+    return octokit.repos.updateBranchProtection({
+        owner: organization,
+        repo: name,
+        branch: branch,
+        enforce_admins: true,
+        required_pull_request_reviews: {
+            dismiss_stale_reviews: true,
+            require_code_owner_reviews: true,
+            required_approving_review_count: 1,
+            require_last_push_approval: true,
+        },
+        required_status_checks: null,
+        restrictions: null,
+    }).then((response) => {
+        core.setOutput("branch_policies", "yes");
+    }).catch((error) => {
+        core.setFailed(error.response.data);
+        core.setOutput("branch_policies", "error");
     });
 
 }
@@ -26,4 +52,9 @@ const name = core.getInput('repository_name');
 const visibility = core.getInput('repository_visibility');
 
 // Main
-createRepository(organization, name, visibility);
+async function run() {
+    await createRepository(organization, name, visibility);
+    await setBranchProtection(organization, name, "main");
+}
+
+run();
