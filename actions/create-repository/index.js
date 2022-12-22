2ae1bfac0015de1e4c8d025846455880b288ffb0
Thomas Dupoiron <tdupoiron@github.com>
2022-12-22 11:23:30 +0100

    add repo name in title

--- actions/create-repository/index.js
+++ actions/create-repository/index.js
@@ -1,19 +1,19 @@
 const core = require('@actions/core');
 const { Octokit } = require("@octokit/rest");
-//const { createAppAuth } = require("@octokit/auth-app");
+const { createAppAuth } = require("@octokit/auth-app");
 
-/*const octokit = new Octokit({
+const octokit = new Octokit({
     authStrategy: createAppAuth,
     auth: {
         appId: process.env.APP_ID,
         privateKey: process.env.PRIVATE_KEY,
         installationId: process.env.INSTALLATION_ID,
     },
-});*/
-
-const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
 });
+
+/*const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+});*/
 
 // Create a new repository
 async function createRepository(organization, name, visibility) {
